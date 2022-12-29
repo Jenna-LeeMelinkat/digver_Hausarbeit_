5649ce9a95307e145dc92f5ad488832716004f0b
Jenna-Lee Melinkat <jenna-lee@MacBook-Pro-von-Jenna-Lee-2.fritz.box>
2022-12-29 15:36:40 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    //dies ist nur ein unnötiger Kommentar mit einem Punkt am Ende.
+}
