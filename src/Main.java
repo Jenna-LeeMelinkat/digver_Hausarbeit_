fe09bde8a65c2998e6b698a41c46f773994a0b56
Jenna-LeeMelinkat <lindenstr31@gmx.de>
2022-12-29 15:57:07 +0100

    versuchtest

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    //dies ist nur ein unnötiger Kommentar mit einem Punkt am Ende.
+    if(0<1){
+
+    }
 }
