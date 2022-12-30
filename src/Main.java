753675b65e4d8ca5b4166601948fc175406b7dfe
Jenna-LeeMelinkat <lindenstr31@gmx.de>
2022-12-30 12:07:35 +0100

    erste Stand

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,54 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import ij.IJ;
+import ij.ImagePlus;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.filter.*;
+
+/**
+ * Die Klasse Hausarbeit_Jenna_Lee_Melinkat_PlugInFilter_ implementiert das Interface
+ * ij.plugin.PlugInFilter. Es soll vorher in ImageJ geladene Bilder analysieren und die Bewegung
+ * und Flugbahn folgend darstellen.
+ *
+ *
+ * @author Jenna-Lee Melinkat
+ */
+
+public class Hausarbeit_Jenna_Lee_Melinkat_PlugInFilter_ implements PlugInFilter {
+    ImagePlus imp;
+
+    /**
+     * Die setup Methode wird aufgerufen, um die Anwendbarkeit des Plugins auf die jeweils ausgewaehlten
+     * Bilder zu ueberpruefen und ist nur auf 8-Bit Graustufenbilder anwendbar.
+     */
+
+    /**
+     * Die Methode Setup wird aufgerufen, zur Initialisierung des Filters.
+     *
+     * @param imp enthält entweder ein ImageProcessor-Objekt oder ein
+     * ImageStack-Objekt
+     * @param arg ein Zeichenkettenargument
+     * @return    Flag, dass die Fähigkeiten des Plug-ins festlegt.
+     */
+    public int setup(String arg, ImagePlus imp) {
+//this.imp = imp;
+        return DOES_8G;	// Der Filter verarbeitet nur 8-Bit-Graustufenbilder anernfalls DOES_ALL
     }
-    if(0<1){
+
+    /**
+     * Die Methode run wird nach setup aufgerufen und erhält als
+     * Parameter den ImageProcessor des zu verarbeitenden Bildes.
+     * Ist statt eines einzelnen Bildes ein ImageStack-Objekt zu filtern,
+     * so wird run für jedes im Stapel enthaltene Bild einzeln aufgerufen.
+     *
+     * @param ip ImageProcessor des zu verarbeitenden Bildes.
+     */
+    public void run(ImageProcessor ip) {
+
+
+
+        public void run(ImageProcessor ip) {
+            ip.invert();
+        }
 
     }
-}
